e4940e3092729d0d363f5dba28abc2a71f036601
Matheus-Godinho <matheus.brc22@gmail.com>
2020-02-05 19:50:10 -0300

    Function writeFile implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,28 @@
 package application;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import model.entities.Item;
 
 public class Program {
 	
 	public static void createFile(File folder, File file) throws IOException {
 		new File(folder.getPath()).mkdir();
 		new File(file.getPath()).createNewFile();
+	}
+	
+	public static void writeFile(List<Item> items, BufferedWriter bw) throws IOException {
+		for (Item i: items) {
+			bw.write(i.toString());
+			bw.newLine();
+		}
 	}
 
 	public static void main(String[] args) {
@@ -25,6 +38,18 @@ public class Program {
 			file = new File(folder.getPath() + "\\file.csv");
 			createFile(folder, file);
 			
+			List<Item> items;
+			
+			items = new ArrayList<>();	
+			items.add(new Item("TV LED", 1290.99, 1));
+			items.add(new Item("Video Game Chair", 350.50, 3));
+			items.add(new Item("Iphone X", 900.00, 2));
+			items.add(new Item("Samsung Galaxy 9", 850.00, 2));
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter(file.getPath()))) {
+				writeFile(items, bw);
+			}
+			items.clear();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}		
@@ -34,3 +59,4 @@ public class Program {
 	}
 
 }
+
