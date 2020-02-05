3684f075be591a961e956ae330e1d2718f28759b
Matheus-Godinho <matheus.brc22@gmail.com>
2020-02-05 18:58:27 -0300

    Item implemented

--- src/model/entities/UpdatedItem.java
+++ src/model/entities/UpdatedItem.java
@@ -1,5 +1,23 @@
 package model.entities;
 
-public class UpdatedItem {
+public class UpdatedItem extends Item {
+	
+	public UpdatedItem() {
+		super();
+	}
+	public UpdatedItem(String name, Double price, Integer quantity) {
+		super(name, price, quantity);
+	}
+	
+	public Double totalValue() {
+		return getPrice() * getQuantity();
+	}
+	
+	@Override
+	public String toString() {
+		return getName() 
+				+ "," 
+				+ String.format("%.2f", totalValue());
+	}
 
 }
