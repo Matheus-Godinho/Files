97cd339b995817420ce928e9ae07d6d9518f3d50
Matheus-Godinho <matheus.brc22@gmail.com>
2020-02-05 19:43:06 -0300

    Function createFile implemented

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,36 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+	
+	public static void createFile(File folder, File file) throws IOException {
+		new File(folder.getPath()).mkdir();
+		new File(file.getPath()).createNewFile();
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		File folder, file;
+		
+		try {
+			System.out.printf("Enter a folder path for the file creation: ");
+			folder = new File(sc.next());
+			file = new File(folder.getPath() + "\\file.csv");
+			createFile(folder, file);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+		sc.close();
+		
 	}
 
 }
